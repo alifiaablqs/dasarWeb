e9adebf49912145ffc4479479454cfe19c6e8e42
alifiaablqs <dyalifiabalqis22@gmail.com>
2024-03-18 10:14:42 +0700

    Pertemuan 5-Fungsi

--- pertemuan5/praktik_php/fungsi.php
+++ pertemuan5/praktik_php/fungsi.php
@@ -1,4 +1,59 @@
 <?php
+//soal 4
+//function perkenalan(){
+//    echo"Assalamualaikum, ";
+//    echo"Perkenalkan, nama saya Dyalifia Balqis Susanto<br/>";
+//    echo"Senang berkenalan dengan Anda<br/>";
+//}
+//memanggil fungsi yang sudah dibuat
+//perkenalan();
+//perkenalan();
+//soal 5
+// membuat fungsi
+//function perkenalan($nama, $salam){
+//    echo $salam.",";
+//    echo"Perkenalkan, nama saya ".$nama."<br/>";
+//    echo"Senang berkenalan dengan Anda<br/>";
+//}
+//memanggil fungsi yang sudah dibuat
+//perkenalan("Hamdana", "Hallo");
+
+//echo "<hr>";
+
+//$saya = "Elok";
+//$ucapanSalam = "Selamat pagi";
+
+//memanggil lagi
+//perkenalan($saya, $ucapanSalam);
+
+//soal 6
+//membuat fungsi
+//function perkenalan($nama, $salam="Assalamualaikum"){
+//    echo $salam.", ";
+//    echo"Perkenalkan, nama saya ".$nama."<br/>";
+//    echo"Senang berkenalan dengan Anda<br/>";
+//}
+//memanggil fungsi yang sudah dibuat
+//perkenalan("Hamdana", "Hallo");
+
+//echo "<hr>";
+
+//$saya = "Elok";
+//$ucapanSalam = "Selamat pagi";
+
+//memanggil lagi tanpa mengisi parameter salam
+//perkenalan($saya);
+
+//soal 7
+//membuat fungsi
+//function hitungUmur($thn_lahir, $thn_sekarang){
+//    $umur = $thn_sekarang - $thn_lahir;
+//    return $umur;
+//}
+
+//echo "Umur saya adalah ". hitungUmur(2003, 2024) ."tahun"
+
+//soal 8
 //membuat fungsi
 function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
