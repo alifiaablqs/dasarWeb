cc2a5a7c43fb8a42b6553f9ff78d9379b255313d
alifiaablqs <dyalifiabalqis22@gmail.com>
2024-04-21 17:40:02 +0700

    Pertemuan 11-Praktikum 5

--- pertemuan11/admin/template/header.php
+++ pertemuan11/admin/template/header.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.118.2">
-    <title>Aplikasi Kantor Siapa</title>
+    <title>Aplikasi Kantor Dyalifia</title>
     <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/dashboard/">
 
